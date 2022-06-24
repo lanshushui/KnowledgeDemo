88ae238da1dcbee75d3c320b05062829ee3bd04a
chentao3 <chentao3@yy.com>
2022-06-24 17:23:05 +0800

    设置图片的透明度从左到右渐变

--- src/UnpopularCode.kt
+++ src/UnpopularCode.kt
@@ -97,3 +97,29 @@ private Object combineArray(Object firstArr, Object secondArr) {
     }
     return newArr;
 }
+
+
+//设置图片的透明度从左到右渐变
+private fun getTransAlphaBitmap(sourceImg: Bitmap, startA: Float, endA: Float): Bitmap {
+    val width = sourceImg.width
+    val height = sourceImg.height
+    val bitArr = IntArray(width * height)
+    sourceImg.getPixels(bitArr, 0, width, 0, 0, width, height)
+    val rangeA = endA - startA
+    return try {
+        for (i in 0 until width) {
+            val progress: Float = i.toFloat() / width.toFloat()
+            val alpha = (startA + rangeA * progress) * 255
+            for (j in 0 until height) {
+                val index = j * width + i
+                val originAlpha = bitArr[index] ushr 24
+                val mixAlpha = originAlpha * alpha / 255
+                bitArr[index] =
+                    mixAlpha.toInt() shl 24 or (bitArr[index] and 0x00FFFFFF)
+            }
+        }
+        Bitmap.createBitmap(bitArr, width, height, Bitmap.Config.ARGB_8888)
+    } catch (e: Throwable) {
+        sourceImg
+    }
+}
